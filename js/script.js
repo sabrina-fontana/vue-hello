c02124a6ccaacae18ea46a3ec0bf44583f25f13e
sabrina-fontana <sabfon@hotmail.it>
2021-01-26 13:40:14 +0100

    esercizio completo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+ el: '#root',
+ data: {
+   title: 'Hello vue',
+   img: 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png'
+ }
+});
+Vue.config.devtools = true;
